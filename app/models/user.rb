39566c73f278501e4429f60910abd1096ef297ae
sonyco-4u <tonyunday@gmail.com>
2023-03-02 17:44:46 +0430

    generate user model for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :categories, class_name: 'Categories', foreign_key: 'user_id'
   has_many :expenses, class_name: 'Expenses', foreign_key: 'user_id'
 
