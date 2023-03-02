449235921ff794fab55bcee9805dba1da0da8d8b
sonyco-4u <tonyunday@gmail.com>
2023-03-02 16:40:10 +0430

    Generate category expenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_120321) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_120837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,9 +21,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_120321) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "category_expenses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "expenses", force: :cascade do |t|
     t.string "name"
     t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_expenses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
