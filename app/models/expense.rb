402a94a678991c72c9ae0a89bc94cd656709e978
sunday_4U <tonyunday@gmail.com>
2023-03-03 22:42:44 +0430

    Generate expenses controller

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,5 +3,5 @@ class Expense < ApplicationRecord
   has_many :category_expenses, class_name: 'CategoryExpense', foreign_key: 'expense_id'
 
   validates :name, presence: true
-  validates :amount, presence: true, numericality: { only_decimal: true, greater_than_or_equal_to: 0 }
+  validates :amount, presence: true, numericality: { only_float: true, greater_than_or_equal_to: 0 }
 end
