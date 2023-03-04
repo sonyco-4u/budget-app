538becb5ebae068ded084a171fb0d630da8ff234
sunday_4U <tonyunday@gmail.com>
2023-03-04 21:19:42 +0430

    Create expenses actions

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,6 +6,10 @@ class ExpensesController < ApplicationController
   def index
   end
 
+  def show
+    @expense = Expense.find(params[:id])
+  end
+
   def new
     @expense = Expense.new
   end
@@ -19,6 +23,35 @@ class ExpensesController < ApplicationController
     else
       flash.now[:alert] = @expense.errors.full_messages.first if @expense.errors.any?
       render :new, status: 400
+    end
+  end
+
+  def edit
+    @expense = Expense.find(params[:id])
+  end
+
+  def update
+    @expense = Expense.find(params[:id])
+    if @expense.update(expense_params)
+      redirect_to category_expense_path(category_id: @category.id, id: @expense.id), notice: 'Expense updated successfully'
+    else
+      flash.now[:alert] = @expense.errors.full_messages.first if @expense.errors.any?
+      render :edit, status: 400
+    end
+  end
+
+  def destroy
+    @expense = Expense.find(params[:id])
+    @category_expenses = CategoryExpense.where(expense_id: @expense.id)
+    @category_expenses.each do |category_expense|
+      expense_id = category_expense.expense_id
+      category_expense.destroy
+    end
+    if @expense.destroy
+      redirect_to category_expenses_path(category_id: @category.id, id: @expense.id), notice: 'Expense deleted successfully'
+    else
+      flash.now[:alert] = @expense.errors.full_messages.first if @expense.errors.any?
+      render :index, status: 400
     end
   end
 
