be9b51f11a9242708afb5c9a54c7ea93743c69b1
sunday_4U <tonyunday@gmail.com>
2023-03-04 19:44:37 +0430

    Add expenses to the route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :home, only: [:index]
-  resources :categories
+  resources :categories do
+    resources :expenses
+  end
   
   # root "articles#index"
   root 'home#index'
