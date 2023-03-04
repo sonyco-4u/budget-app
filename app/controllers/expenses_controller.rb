4de69e53159379568c930c30ce63aeb330669ac9
sunday_4U <tonyunday@gmail.com>
2023-03-04 19:28:52 +0430

    Style categories

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,6 +6,22 @@ class ExpensesController < ApplicationController
   def index
   end
 
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    @expense.user = @user
+    if @expense.save
+      CategoryExpense.create(category_id: @category.id, expense_id: @expense.id)
+      redirect_to category_expenses_path(category_id: @category.id), notice: 'Expense created successfully'
+    else
+      flash.now[:alert] = @expense.errors.full_messages.first if @expense.errors.any?
+      render :new, status: 400
+    end
+  end
+
   private
 
   def find_user
