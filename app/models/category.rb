1a2097dd55283464ea837d746c4c757e18e9d48b
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-03-06 13:31:52 -0800

    Add comment for code clarrification

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,6 @@
 # It inherits from the ApplicationRecord class which provides a base implementation for all ActiveRecord models.
 # It defines a 'belongs_to' association with the 'User' model, using the foreign key 'user_id',
 # to establish a relationship between a category and the user who created it.
-# It also defines a 'has_many' association with the 'CategoryExpense' model, and a 'has_many through' association with the 'Expense' model,
-# to establish a many-to-many relationship between categories and expenses.
 class Category < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :category_expenses
